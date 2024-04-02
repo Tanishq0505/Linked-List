08da42e1ee0b9fee20fa86fdbbd57c358cc6b006
Tanishq0505 <126601008+Tanishq0505@users.noreply.github.com>
2024-04-02 21:19:13 +0530

    Create 13_Reverse_Linked_List.cpp

---
+++ 13_Reverse_Linked_List.cpp
@@ -0,0 +1,25 @@
+// 206 Leetcode
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+class Solution {
+public:
+    // https://youtu.be/vqS1nVQdCJM?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA
+    // approach 1 from lec 45
+    ListNode* reverseList(ListNode* head) {
+
+if(head==NULL||head->next==NULL){
+    return head;
+}
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+            
+        }
+        return prev;
+    }
+};
