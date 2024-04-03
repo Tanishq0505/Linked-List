47db7fbfc1ef441c9f510fa146ebee361dd0cc8b
Tanishq0505 <126601008+Tanishq0505@users.noreply.github.com>
2024-04-03 18:43:26 +0530

    Create 24_Remove_Duplicates_from_Sorted_List.cpp

---
+++ 24_Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,34 @@
+// 83. Remove Duplicates from Sorted List
+// Leetcode 
+// see code &copy to understand 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp=head;
+        if(head==NULL){
+            return NULL;
+        }
+     
+        while(temp!=NULL){
+            if(temp->next!=NULL&&temp->val==temp->next->val){
+        
+                temp->next=temp->next->next;  
+            }
+            else{
+             temp=temp->next;
+            }
+
+        }
+        return head;
+    }
+};
